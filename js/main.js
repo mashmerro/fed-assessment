ce0b7911720475b738a1a0d01c83dce6a7366dd3
mashmerro <82529907+mashmerro@users.noreply.github.com>
2022-01-23 21:53:17 -0600

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,64 @@
-const accordion = document.getElementsByClassName("expander");
+// SLIDESHOW
+let slideIndex = 0;
+showSlides();
+var slides,dots;
 
-for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+function plusSlides(position) {
+    slideIndex += position;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    else if(slideIndex < 1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+
+      }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+    }
+
+function currentSlide(index) {
+    if (index > slides.length) {index = 1}
+    else if(index < 1){index = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+        slides[index-1].style.display = "block";  
+        dots[index-1].className += " active";
+    }
+
+function showSlides() {
+    let i;
+    slides = document.getElementsByClassName("slideshow");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); 
+}
+
+// ACCORDION
+const accordion = document.getElementsByClassName("expander__toggle");
+
+  for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+      this.classList.toggle("current");
+      const panel = this.nextElementSibling;
+      if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
   });
 }
